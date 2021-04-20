0f24862a41ea50cc8a6ac4878d63131ffbc19035
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2021-04-20 14:27:38 +0200

    Improve comments and add documentation

--- summarizer/process_reviews.py
+++ summarizer/process_reviews.py
@@ -1,33 +1,67 @@
+# Created by bugra-yilmaz on 08.02.2021.
+#
+# Review processing script to collecting reviews of each restaurant, summarizing them and storing the results
+
+# Imports
 import re
 import os
-import time
 import json
 import logging
 import argparse
 from collections import Counter
 
+from summarizer import Summary
 from summarizer import Summarizer
 
 
-def clean_review(text):
+def clean_review(text: str):
+    """
+    Removes a set of special characters from the review text for the sake of dependency parsing.
+
+    Parameters:
+    text (str): Review text.
+
+    Returns:
+    str: Cleaned review text.
+
+    """
     return re.sub(r'[$%+^&@#*]+', r'', text)
 
 
-def extract_reason(summaries, n_features=3):
+def extract_reason(summaries: list[Summary], n_features: int = 3):
+    """
+    Extracts the reason from summarized review list.
+    Returns the most frequent features and most frequent opinions about those features as seen in the review texts.
+
+    Parameters:
+    summaries (list[Summary]): List of summaries of review texts. Basically a list of opinion-feature pair lists.
+
+    Returns:
+    str: Reason string, constructed from most frequent feature and opinions. Example: "nice food, good service".
+
+    """
     features = dict()
+    # Collect opinions belonging to each feature
     for summary in summaries:
         for opinion, feature in summary:
             if feature in features:
                 features[feature].append(opinion)
             else:
                 features[feature] = [opinion]
+
+    # Get n most frequent features of a restaurant
     top_features = sorted(features.keys(), key=lambda x: len(features[x]), reverse=True)[:n_features]
+    # Get the most frequent opinion for each selected feature
     top_opinions = [Counter(features[feature]).most_common()[0][0] for feature in top_features]
+
+    # Construct the reason with the most frequent features and corresponding opinions
     reason = [' '.join([opinion, feature]) for opinion, feature in zip(top_opinions, top_features)]
+
     return ','.join(reason)
 
 
 if __name__ == '__main__':
+    # Parse command line arguments
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument('-ybd', '--yelp-bus-data', help='Filepath to Yelp business dataset',
                                  dest='ybd', default='data/sample_business.json', metavar='')
@@ -43,52 +77,67 @@ if __name__ == '__main__':
                                  default='restaurants.json', dest='o', metavar='')
     args = argument_parser.parse_args()
 
+    # Format logging
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)7s: %(message)s',
                         datefmt='%d-%m-%Y %H:%M:%S')
 
     logging.info('Collecting restaurants data...')
-    t = time.time()
+    # Collect data for n restaurants from the given business data
     restaurants = dict()
     with open(args.ybd, 'r') as f:
         for line in f:
             business = json.loads(line)
+            # Check if the business is a "Restaurant"
             if business['categories'] and 'Restaurants' in business['categories'].split(', '):
                 restaurants[business['business_id']] = {'name': business['name'], 'rating': business['stars'],
                                                         'categories': business['categories'], 'reviews': set()}
+                # Stop after collecting n restaurants
                 if len(restaurants) == args.n:
                     break
-    logging.info(f'Collected {len(restaurants)} restaurants - {round(time.time() - t, 2)} seconds.')
+    logging.info(f'Collected {len(restaurants)} restaurants.')
 
     logging.info('Collecting and summarizing reviews data...')
-    t = time.time()
+    # Collect review data belonging to already collected n restaurants
     reviews = dict()
+    # Initialize the Stanford summarizer
     summarizer = Summarizer(args.sj, args.smj)
     with open(args.yrd, 'r') as f:
         for line in f:
             review = json.loads(line)
-            if review['business_id'] in restaurants:
-                rating = review['stars']
-                review_id, restaurant_id = review['review_id'], review['business_id']
 
-                if rating > restaurants[restaurant_id]['rating']:
-                    continue
+            # Do not collect reviews belonging to other businesses
+            if review['business_id'] not in restaurants:
+                continue
 
-                text = clean_review(review['text'])
-                summary = summarizer.summarize(text)
-                reviews[review_id] = {'rating': rating, 'summary': summary}
-                restaurants[restaurant_id]['reviews'].add(review_id)
+            rating = review['stars']
+            review_id, restaurant_id = review['review_id'], review['business_id']
 
+            # Consider only ratings with lower than or equal to average rating
+            if rating > restaurants[restaurant_id]['rating']:
+                continue
+
+            # Clean the review text and summarize it - get opinion-feature pairs from it
+            text = clean_review(review['text'])
+            summary = summarizer.summarize(text)
+
+            reviews[review_id] = {'rating': rating, 'summary': summary}
+            restaurants[restaurant_id]['reviews'].add(review_id)
+
+    # Stop the Stanford summarizer
     summarizer.stop()
-    logging.info(f'Collected and summarized {len(reviews)} reviews - {round(time.time() - t, 2)} seconds.')
+    logging.info(f'Collected and summarized {len(reviews)} reviews.')
 
+    # For each restaurant, collect summaries of reviews and extract the "reason"
     for restaurant, data in restaurants.items():
         summaries = list()
         for review_id in data['reviews']:
             summary = reviews[review_id]['summary']
             summaries.append(summary)
+        # Get reason from collected summaries
         reason = extract_reason(summaries)
         restaurants[restaurant]['reason'] = reason
 
+    # Write the output to a .json file with all the information needed in next steps
     if not os.path.exists('output'):
         os.mkdir('output')
     with open(os.path.join('output', args.o), 'w') as f:
--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -1,3 +1,8 @@
+# Created by bugra-yilmaz on 08.02.2021.
+#
+# Summarizer implementation to extract opinion-feature pairs from a review text
+
+# Imports
 import logging
 from requests.exceptions import HTTPError
 
@@ -5,35 +10,74 @@ from nltk.parse.corenlp import CoreNLPServer
 from nltk.parse.corenlp import CoreNLPServerError
 from nltk.parse.corenlp import CoreNLPDependencyParser
 
+# Define type aliases for review summarization
+Remark = tuple[str, str]  # opinion-feature pair, example: ('nice', 'food')
+Summary = list[Remark]  # list of remarks, i.e. opinion-feature pairs
+
 
 class Summarizer:
+    """
+    Summarizer class implementing opinion-feature extraction. Uses Stanford CoreNLP dependency parser.
+
+    Attributes:
+    server (CoreNLPServer): CoreNLP server for accessing Stanford CoreNLP services.
+    parser (CoreNLPDependencyParser): CoreNLP dependency parser.
+
+    """
     def __init__(self, jar_path, models_jar_path):
+        """
+        The constructor for Summarizer class.
+
+        Parameters:
+        jar_path (str): Filepath to Stanford CoreNLP .jar file.
+        models_jar_path (str): Filepath to Stanford CoreNLP models .jar file.
+
+        """
         logging.info('Starting CoreNLP server...')
         self.server = CoreNLPServer(path_to_jar=jar_path, path_to_models_jar=models_jar_path)
         try:
             self.server.start()
             logging.info('CoreNLP server started.')
+        # CoreNLPServerError is thrown when a server is already running
         except CoreNLPServerError:
             logging.warning('CoreNLP server is already running.')
         self.parser = CoreNLPDependencyParser()
 
     def summarize(self, text):
+        """
+        Summarizes a review. Extracts opinion-feature pairs from it.
+
+        Parameters:
+        text (str): Review text.
+
+        Returns:
+        Summary: List of opinion-feature pairs extracted from the review text.
+
+        """
         try:
             parse = next(self.parser.raw_parse(text))
+        # An HTTPError raised by the CoreNLP server is related to unrecognized characters in the review text
         except HTTPError:
             logging.warning(f'Review skipped: {text}')
             return []
 
+        # Search dependency parsing result to find "nsubj" or "amod" tags
         summary = list()
         for governor, dep, dependent in parse.triples():
             if dep == 'nsubj':
+                # Look if the nominal subject is noun and if it is modified by an adjective
                 if governor[1] == 'JJ' and dependent[1] in {'NN', 'NNS'}:
                     summary.append((governor[0].lower(), dependent[0].lower()))
             elif dep == 'amod':
+                # Look if the adjective is linked to a noun
                 if dependent[1] == 'JJ' and governor[1] in {'NN', 'NNS'}:
                     summary.append((dependent[0].lower(), governor[0].lower()))
         return summary
 
     def stop(self):
+        """
+        Stops the CoreNLP server of the summarizer object.
+
+        """
         self.server.stop()
         logging.info('CoreNLP server stopped.')
