f04728e24c0c647d2f7487f203e1aa30db2d981e
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2021-04-21 11:26:54 +0200

    Improve unit tests

--- services/app/test.py
+++ services/app/test.py
@@ -6,33 +6,40 @@
 import unittest
 import requests
 
+# Define constant URL string
+URL = 'http://localhost:5000'
+
 
 class BasicTestCase(unittest.TestCase):
-    # Test main URL for returned text
+    # Test main endpoint for returned text
     def test_main_url(self):
-        response = requests.get('http://localhost:5000')
+        response = requests.get(URL)
+        self.assertEqual(response.status_code, 200, msg='Status code is not OK for main endpoint.')
         self.assertEqual(response.text, 'Welcome to the REST API!',
-                         msg='Main url "/" does not return the expected placeholder text.')
+                         msg='Main endpoint does not return the expected placeholder text.')
 
-    # Test the category parameter for case insensitivity
+    # Test the restaurants endpoint for case insensitivity
     def test_case_sensitivity(self):
-        response_lower = requests.get('http://localhost:5000/restaurants?category=burgers')
-        response_upper = requests.get('http://localhost:5000/restaurants?category=BURGERS')
-        response_mixed = requests.get('http://localhost:5000/restaurants?category=bUrGERs')
+        response_lower = requests.get(URL + '/restaurants?category=burgers')
+        response_upper = requests.get(URL + '/restaurants?category=BURGERS')
+        response_mixed = requests.get(URL + '/restaurants?category=bUrGERs')
         self.assertEqual(response_lower.json(), response_upper.json(),
                          msg='Case insensitivity is not satisfied for category search.')
         self.assertEqual(response_lower.json(), response_mixed.json(),
                          msg='Case insensitivity is not satisfied for category search.')
 
-    # Test the response structure to see if it contains the needed fields
+    # Test the restaurants endpoint for response structure
     def test_response_structure(self):
-        response = requests.get('http://localhost:5000/restaurants?category=italian')
+        response = requests.get(URL + '/restaurants?category=italian')
+
+        self.assertEqual(response.status_code, 200, msg='Status code is not OK for restaurants endpoint.')
+
         key = 'restaurants'
         self.assertTrue(key in response.json(), msg=f'Response does not contain the key: {key}.')
 
         fields = ('business_id', 'name', 'rating', 'reason')
         fields_check = all(field in response.json()['restaurants'][0] for field in fields)
-        self.assertTrue(fields_check, msg=f'Fields of a restaurant object are not: {fields}.')
+        self.assertTrue(fields_check, msg=f'One or more required fields is missing in the restaurant object.')
 
 
 # Run the unit tests
